ef1bb8370680ac23666ae5a56b5cdac1f9848e53
Radoslav Georgiev <g2h.horde@gmail.com>
2014-04-10 17:42:51 +0300

    Refactoring sql_manager.py from module to class

--- sql_manager.py
+++ sql_manager.py
@@ -1,48 +1,51 @@
-import requests
 import sqlite3
-from Client import Client
-
-conn = sqlite3.connect("bank.db")
-cursor = conn.cursor()
+from client import Client
 
 
-def create_clients_table():
-    create_query = '''create table if not exists
-        clients(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT,
-                password TEXT,
-                balance REAL DEFAULT 0,
-                message TEXT)'''
+class SqlManager():
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
 
-    cursor.execute(create_query)
+    def create_clients_table(self):
+        create_query = '''create table if not exists
+            clients(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT,
+                    password TEXT,
+                    balance REAL DEFAULT 0,
+                    message TEXT)'''
 
+        self.cursor.execute(create_query)
 
-def change_message(new_message, logged_user):
-    update_sql = "UPDATE clients SET message = '%s' WHERE id = '%s'" % (new_message, logged_user.get_id())
-    cursor.execute(update_sql)
-    conn.commit()
-    logged_user.set_message(new_message)
+    def change_message(self, new_message, logged_user):
+        update_sql = """UPDATE clients
+                        SET message = '%s'
+                        WHERE id = '%s'"""
+        update_sql = update_sql % (new_message, logged_user.get_id())
+        self.cursor.execute(update_sql)
+        self.conn.commit()
+        logged_user.set_message(new_message)
 
+    def change_pass(self, new_pass, logged_user):
+        update_sql = """UPDATE clients
+                        SET password = '%s'
+                        WHERE id = '%s'"""
+        update_sql = update_sql % (new_pass, logged_user.get_id())
+        self.cursor.execute(update_sql)
+        self.conn.commit()
 
-def change_pass(new_pass, logged_user):
-    update_sql = "UPDATE clients SET password = '%s' WHERE id = '%s'" % (new_pass, logged_user.get_id())
-    cursor.execute(update_sql)
-    conn.commit()
+    def register(self, username, password):
+        insert_sql = "insert into clients (username, password) values ('%s', '%s')" % (username, password)
+        self.cursor.execute(insert_sql)
+        self.conn.commit()
 
+    def login(self, username, password):
+        select_query = "SELECT id, username, balance, message FROM clients WHERE username = '%s' AND password = '%s' LIMIT 1" % (username, password)
 
-def register(username, password):
-    insert_sql = "insert into clients (username, password) values ('%s', '%s')" % (username, password)
-    cursor.execute(insert_sql)
-    conn.commit()
+        self.cursor.execute(select_query)
+        user = self.cursor.fetchone()
 
-
-def login(username, password):
-    select_query = "SELECT id, username, balance, message FROM clients WHERE username = '%s' AND password = '%s' LIMIT 1" % (username, password)
-    
-    cursor.execute(select_query)
-    user = cursor.fetchone()
-
-    if(user):
-        return Client(user[0], user[1], user[2], user[3])
-    else:
-        return False
+        if(user):
+            return Client(user[0], user[1], user[2], user[3])
+        else:
+            return False
